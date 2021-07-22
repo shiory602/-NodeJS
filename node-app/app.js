6ed2e21f51194bcff6b62d9cc958ef91ac636ece
shiory602 <suzushioufo@gmail.com>
2021-07-22 16:54:45 -0700

    readFile

--- node-app/app.js
+++ node-app/app.js
@@ -1,16 +1,36 @@
+// list 2-1
+// const http = require('http');
+
+// var server = http.createServer(
+//     (request, response) => {
+//         response.setHeader('Content-Type', 'text/html');
+//         response.write('<!DOCTYPE html><html lang="en">');
+//         response.write('<head><meta charset="utf-8">');
+//         response.write('<title>Hello</title></head>');
+//         response.write('<body><h2>Hello, world!</h2>');
+//         response.write('<p>This is Node.js sample page</p>');
+//         response.write('<p>This is Node.js sample page</p>', 'utf8');
+//         response.write('</body></html>');
+//         response.end();
+//     }
+// );
+
+// server.listen(3000, console.log('server start!'))
+
+
+
+// list 2-5
 const http = require('http');
+const fs = require('fs');
 
 var server = http.createServer(
     (request, response) => {
-        response.setHeader('Content-Type', 'text/html');
-        response.write('<!DOCTYPE html><html lang="en">');
-        response.write('<head><meta charset="utf-8">');
-        response.write('<title>Hello</title></head>');
-        response.write('<body><h2>Hello, world!</h2>');
-        response.write('<p>This is Node.js sample page</p>');
-        response.write('<p>This is Node.js sample page</p>', 'utf8');
-        response.write('</body></html>');
-        response.end();
+        fs.readFile('./index.html', 'UTF-8',
+            (err, data) => {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write(data);
+                response.end()
+            });
     }
 );
 
