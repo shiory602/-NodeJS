14165ce321387855aad21ceb8d42c6ba9be23467
shiory602 <suzushioufo@gmail.com>
2021-07-22 18:28:12 -0700

    EJS

--- node-app/README.md
+++ node-app/README.md
@@ -17,3 +17,50 @@ fs.readFile(読み込むファイルパス, ファイルの内容のエンコー
 ```js
 (error, data) => { ...処理... }
 ```
+
+## テキストの置換
+`replace`はテキストの中から第一引数の検索テキストを探し出し、それを第二引数の置換テキストに置き換える
+```
+変数 = テキスト .replace( 検索テキスト, 置換テキスト );
+```
+### 正規表現
+テキストの繋がりを表す「パターン」を元に検索を行う正規表現を使う
+- `g`はグローバル検索を行う
+```
+/ ... /g
+```
+[正規表現MDN](https://developer.mozilla.org/ja/docs/orphaned/Web/JavaScript/Guide/Regular_Expressions)
+
+
+# テンプレートエンジン
+テンプレートを使って表示するコンテンツを用意するための仕組み
+### EJS
+JavaScriptで利用するシンプルなテンプレートエンジン
+**Embedded JavaScript Templates**の略称のことを言い、Node.jsではpackageマネージャーにインストールする
+インストール方法は下記コマンドをターミナルに入力
+```
+npm install -g ejs
+```
+## EJSファイルの表示方法
+1. テンプレートファイルを読み込む
+```js
+const index_page = fs.readFileSync('./index.ejs', 'utf8')
+```
+2. レンダリングする
+テンプレートの内容を元に、実際に表示されるHTMLのソースコードを生成する
+（テンプレート自体がHTMLの時は行わなくても良い）
+```js
+var content = ejs.render(index_page);
+```
+3. 生成された表示内容を出力する
+生成された表示内容を`write`などを使って出力する
+```js
+response.writeHead(200, {'Content-Type': 'text/html'});
+response.write(content);
+response.end()
+```
+### Error: Cannot find module 'ejs'が表示されたら
+ターミナルでアプリのファイルまで移動して`npm init`をした後に下記コマンドを実行
+```
+npm install --save ejs
+```
--- node-app/app.js
+++ node-app/app.js
@@ -38,24 +38,107 @@
 
 
 
-// list 2-6
+// // list 2-6
+// const http = require('http');
+// const fs = require('fs');
+
+// var server = http.createServer(getFromClient)
+// server.listen(3000);
+// console.log('server start!');
+
+// // ここまでメインプログラム ==================
+
+// // CreateServerの処理
+//     function getFromClient(req, res) {
+//         request = req;
+//         response = res;
+//         fs.readFile('./index.html', 'UTF-8',
+//             (err, data) => {
+//                 response.writeHead(200, {'Content-Type': 'text/html'});
+//                 response.write(data);
+//                 response.end()
+//             });
+//     }
+
+
+
+// // list 2-7
+// const http = require('http');
+// const fs = require('fs');
+// var request;
+// var response;
+
+// var server = http.createServer(getFromClient);
+
+// server.listen(3000);
+// console.log('server start!');
+
+// // ここまでメインプログラム ==================
+
+// // CreateServerの処理
+// function getFromClient(req, res) {
+//     request = req;
+//     response = res;
+//     fs.readFile('./index.html', 'UTF-8', writeToResponse)
+// }
+
+// // readFile完了後の処理
+// function writeToResponse(err, data) {
+//     response.writeHead(200, {'Content-Type': 'text/html'});
+//     response.write(data);
+//     response.end()
+// }
+
+
+
+
+// // list 2-9
+// const http = require('http');
+// const fs = require('fs');
+
+// var server = http.createServer(getFromClient);
+
+// server.listen(3000);
+// console.log('server start!');
+
+// // ここまでメインプログラム ==================
+
+// // CreateServerの処理
+// function getFromClient(request, response) {
+//     fs.readFile('./index.html', 'UTF-8',
+//         (error, data) => {
+//             var content = data
+//             .replace(/dummy_title/g, 'This is Title')
+//             .replace(/dummy_content/g, 'This is Content');
+
+//             response.writeHead(200, {'Content-Type': 'text/html'});
+//             response.write(content);
+//             response.end()
+//         }
+//     );
+// }
+
+
+
+
+// list 2-11
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
 
-var server = http.createServer(getFromClient)
+const index_page = fs.readFileSync('./index.ejs', 'utf8')
+
+var server = http.createServer(getFromClient);
+
 server.listen(3000);
 console.log('server start!');
 
 // ここまでメインプログラム ==================
 
 // CreateServerの処理
-    function getFromClient(req, res) {
-        request = req;
-        response = res;
-        fs.readFile('./index.html', 'UTF-8',
-            (err, data) => {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.write(data);
-                response.end()
-            });
-    }
+function getFromClient(request, response) {
+    var content = ejs.render(index_page);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(content);
+    response.end()
+}
